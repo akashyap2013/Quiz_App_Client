e7e2aaebea6dd39cc6764ecb1103429eb634fedf
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-10-15 10:57:39 +0530

    Quiz Component Added

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
 export default function Quiz() {
+
+
+    function onNext(){
+        console.log('On next click')
+    }
+
+    function onPrev(){
+        console.log('On onPrev click')
+    }
+
   return (
-    <div>Quiz</div>
+    <div className='container'>
+        <h1 className='title text-light'>Quiz Application</h1>
+
+        {/* display questions */}
+
+        <div className='grid'>
+            <button className='btn prev' onClick={onPrev}>Prev</button>
+            <button className='btn next' onClick={onNext}>Next</button>
+        </div>
+    </div>
   )
 }
